bf58e7ec4cac70b20cf6f0403698ae85dd07b712
songye38 <vsongyev@hanmail.net>
2017-10-13 17:58:01 +0900

    add zs-042 module to check exact time

--- read_light_value/read_light_value.ino
+++ read_light_value/read_light_value.ino
@@ -1,11 +1,15 @@
-
+#include <Wire.h>
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <SD.h>
 
+
 // set up variables using the SD utility library functions:
 File dataFile;
 const int chipSelect = 6;
+
+//set up for zs-042 module
+#define DS3231_I2C_ADDRESS 0x68
 
 //button 
 int mode =-1;
@@ -27,6 +31,7 @@ void setup() {
   pinMode(sw, INPUT_PULLUP);
   lcd.begin(16, 2);
   initialize_sd();
+  initialize_time();
 }
 
 //다시 처음으로 돌아가는 모드 추가 여기에 나중에 부저를 추가하면 더 좋다 
@@ -43,6 +48,7 @@ void loop() {
         display_onoff(mode);
         int cds_value = read_cds_value();
         write_to_sd(cds_value);
+        displayTime();
       }
       else if(mode==-1)
       {
@@ -288,5 +294,94 @@ void initialize_sd()
   }
   Serial.println("initialization done.");
 }
+
+
+byte decToBcd(byte val)
+{
+return( (val/10*16) + (val%10) );
+}
+
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return( (val/16*10) + (val%16) );
+}
+
+void initialize_time()
+{
+  Wire.begin();
+  //현재 시간을 정하는 부분
+  Serial.begin(9600);
+  setDS3231time(00,55,9,11,10,17); 
+  Serial.println("time set complete");
+}
+//시계의 날짜를 정하는 함수
+void setDS3231time(byte second, byte minute, byte hour, byte dayOfMonth, byte month, byte year)
+{
+  // sets time and date data to DS3231
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set next input to start at the seconds register
+  Wire.write(decToBcd(second)); // set seconds
+  Wire.write(decToBcd(minute)); // set minutes
+  Wire.write(decToBcd(hour)); // set hours
+  //Wire.write(decToBcd(dayOfWeek)); // set day of week (1=Sunday, 7=Saturday)
+  Wire.write(decToBcd(dayOfMonth)); // set date (1 to 31)
+  Wire.write(decToBcd(month)); // set month
+  Wire.write(decToBcd(year)); // set year (0 to 99)
+  Wire.endTransmission();
+}
+
+void readDS3231time(byte *second,
+  byte *minute,
+  byte *hour,
+  byte *dayOfMonth,
+  byte *month,
+  byte *year)
+{
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set DS3231 register pointer to 00h
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
+  // request seven bytes of data from DS3231 starting from register 00h
+  *second = bcdToDec(Wire.read() & 0x7f);
+  *minute = bcdToDec(Wire.read());
+  *hour = bcdToDec(Wire.read() & 0x3f);
+  *dayOfMonth = bcdToDec(Wire.read());
+  *month = bcdToDec(Wire.read());
+  *year = bcdToDec(Wire.read());
+}
+
+void displayTime()
+{
+byte second, minute, hour, dayOfMonth, month, year;
+readDS3231time(&second, &minute, &hour, &dayOfMonth, &month,
+&year);
+// send it to the serial monitor
+Serial.print(hour, DEC);
+// convert the byte variable to a decimal number when displayed
+Serial.print(":");
+if (minute<10)
+{
+Serial.print("0");
+}
+Serial.print(minute, DEC);
+Serial.print(":");
+if (second<10)
+{
+Serial.print("0");
+}
+Serial.print(second, DEC);
+Serial.print(" ");
+
+Serial.print(year , DEC);
+Serial.print("/");
+Serial.print(month, DEC);
+Serial.print("/");
+Serial.print(dayOfMonth, DEC);
+Serial.print(" ");
+
+}
+
+
 
 
